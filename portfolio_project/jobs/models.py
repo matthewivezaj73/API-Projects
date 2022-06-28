ecf5119fa68d84ae6dca3c60a8e07ab1093f76e1
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-28 07:36:38 -0400

    added images/

--- portfolio_project/jobs/models.py
+++ portfolio_project/jobs/models.py
@@ -5,6 +5,6 @@ class Job(model.Model):
         Creating a Job class from django.db.models
     """
     #Creating a variable that takes a parameter of where to upload the object.
-    image = models.ImageField(upload_to='')
+    image = models.ImageField(upload_to='images/')
     #Creating a summary that creates a charfield.
     summary = models.CharField(max_length=200)
