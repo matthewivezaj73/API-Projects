e927c60d9df76c51d11aadf1ef09f0c7156b7aaa
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-29 08:41:16 -0400

    Added a database.

--- portfolio-project/jobs/models.py
+++ portfolio-project/jobs/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-class Job(models.Model)
+class Job(models.Model):
+    """
+        A class that allows the object to 
+        be saved inside of the database.
+    """
