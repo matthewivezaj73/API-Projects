e8a52cc0c7d329d768dfe86946613f571dc6860d
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-29 08:46:34 -0400

    added a comment.

--- portfolio-project/jobs/models.py
+++ portfolio-project/jobs/models.py
@@ -9,3 +9,4 @@ class Job(models.Model):
     """
     #Assigning a place for the user to upload an image to a variable.
     image = models.ImageField(upload_to='')
+    #Assigning a summary to a variable.
