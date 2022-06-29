12828fcc72dd048b90b8da889534b1831004c6a2
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-29 07:27:00 -0400

    added images path.

--- portfolio_project/jobs/models.py
+++ portfolio_project/jobs/models.py
@@ -8,6 +8,6 @@ class Job(models.Model):
         to be saved inside of the database.
     """
     #Accepting an image that the user can upload.
-    image = models.ImageField(upload_to='')
+    image = models.ImageField(upload_to='images/')
     #Creating a charfield.
     summary = models.CharField(max_length=200)
