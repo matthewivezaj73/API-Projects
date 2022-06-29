a4f4df25e051257a6c39883bdbd72fb03ca9c9fc
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-29 07:20:43 -0400

    Added a docstring.

--- portfolio_project/jobs/models.py
+++ portfolio_project/jobs/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 #Creating a class.
 class Job(models.Model):
-    
+    """
+        A class that allows for the job 
+        to be saved inside of the database.
+    """
